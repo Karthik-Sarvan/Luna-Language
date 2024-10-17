8d3a3a880812bdabcd068c762a3106e8018883a7
Karthik-Sarvan <karthiks1mail@gmail.com>
2024-10-17 18:19:56 +0000

    added some files and reading input

---
+++ build/CMakeFiles/luna.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/luna.dir/src/main.cpp.o"
+  "CMakeFiles/luna.dir/src/main.cpp.o.d"
+  "luna"
+  "luna.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/luna.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.asm
@@ -0,0 +1,6 @@
+global _start
+
+start:
+    mov rax, 60
+    mov rdi, 69
+    syscall
